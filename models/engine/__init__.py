2feca1a16456ab5fa4cad5d62b70dd1d3eb65d82
ossaichidozie <112661257+hoodiboiy@users.noreply.github.com>
2023-11-13 20:47:03 +0200

    uppppt air

---
+++ models/engine/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Upon import, this package initializes, populating all objects in its instance private attribute __objects
+"""
+from .engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
